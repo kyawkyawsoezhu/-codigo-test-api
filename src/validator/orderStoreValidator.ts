d0a456ceeeb0e17e6a3db571d5143a2dca421ee9
Kyaw Kyaw Soe <kyawkyawsoezhu@soefamily>
2021-07-04 23:08:04 +0630

    validate creditCardNumber

--- src/validator/orderStoreValidator.ts
+++ src/validator/orderStoreValidator.ts
@@ -2,7 +2,8 @@ const { body, validationResult, } = require('express-validator')
 
 export default [
     body('voucherId').notEmpty().isInt(),
-    body('paymentMethod').notEmpty().isIn(['cash','creditCard']),
+    body('paymentMethod').notEmpty().isIn(['cash', 'creditCard']),
+    body('creditCardNumber').if((value, { req }) => req.body.paymentMethod == 'creditCard').notEmpty().isCreditCard(),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
