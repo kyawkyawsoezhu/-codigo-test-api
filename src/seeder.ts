12a4ad9cf1ad4f8fe96a09e7fbc8ef1858a2637c
Kyaw Kyaw Soe <kyawkyawsoezhu@soefamily>
2021-07-04 20:59:54 +0630

    update seed

--- src/seeder.ts
+++ src/seeder.ts
@@ -8,15 +8,22 @@ import { Voucher } from "./entity/Voucher";
 createConnection().then(async connection => {
 
     console.info("Seeding User Data...");
-    // await Promise.all(range(5).map(async () => {
-    //     const password = await bcrypt.hash('password', 10);
-    //     await connection.manager.save(connection.manager.create(User, {
-    //         name: faker.name.findName(),
-    //         username: faker.unique(faker.internet.userName),
-    //         password,
-    //         phoneNumber: faker.unique(faker.phone.phoneNumber),
-    //     }))
-    // }))
+    await connection.manager.save(connection.manager.create(User, {
+        name: 'Admin',
+        username: 'admin',
+        password: await bcrypt.hash('password', 10),
+        phoneNumber: faker.unique(faker.phone.phoneNumber),
+    }))
+
+    await Promise.all(range(5).map(async () => {
+        const password = await bcrypt.hash('password', 10);
+        await connection.manager.save(connection.manager.create(User, {
+            name: faker.name.findName(),
+            username: faker.unique(faker.internet.userName),
+            password,
+            phoneNumber: faker.unique(faker.phone.phoneNumber),
+        }))
+    }))
 
     console.info("Seeding Vocher Data...");
     await Promise.all(range(5).map(async () => {
@@ -25,14 +32,14 @@ createConnection().then(async connection => {
             title: `${amount}$`,
             description: faker.lorem.word(5),
             expiredAt: faker.date.between('2022-01-01', '2023-01-01'),
-            image: faker.image.dataUri(),
+            image: faker.random.image(),
             amount: amount,
             quantity: faker.datatype.number({ min: 10, max: 100 }),
             isActive: faker.datatype.boolean(),
             // type: faker.random.arrayElement(["myself", "gift"]), 
             type: "gift",
             limitPerUser: faker.datatype.number({ min: 2, max: 10 }),
-            // discountPayment: faker.random.arrayElement(["creditCards", "cash"]),
+            // discountPayment: faker.random.arrayElement(["creditCard", "cash"]),
             discountPayment: "cash",
             discountValue: faker.datatype.number({ min: 5, max: 20 }),
         }))
